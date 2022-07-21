90319f9ef21e1a4901de9741a634af2a03e6cc0c
alpeshmail <alpeshmail@gmail.com>
2022-07-21 14:08:32 -0400

    Heap Algorithm

---
+++ Project_6/Algorithm/Heap/MinimiseCostConnectRopes.cs
@@ -0,0 +1,53 @@
+﻿namespace Algorithm.Heap
+{
+    public class MinimiseCostConnectRopes
+    {
+        /// <summary>
+        /// This is question was asked amazon
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int GetMinCostUsedPriorityQueue(int[] arr,int n)
+        {
+            int totalCost = 0;
+            PriorityQueue<int,int> priorityQueue = new();
+
+            for(int i = 0; i < n; i++)
+                priorityQueue.Enqueue(arr[i], arr[i]);
+
+            do
+            {
+                int minRope1 = priorityQueue.Dequeue();
+                int minRope2 = priorityQueue.Dequeue();
+                int newRopeCost = minRope1 + minRope2;
+                totalCost += newRopeCost;
+                priorityQueue.Enqueue(newRopeCost, newRopeCost);
+
+            } while(priorityQueue.Count > 1);
+
+            return totalCost;
+        }
+
+        public int GetMinCostUseMinHeap(int[] arr, int n)
+        {
+            int totalCost = 0;
+            MinHeap minHeap = new MinHeap(n);
+
+            for (int i = 0; i < n; i++)
+                minHeap.Add(arr[i]);
+
+            do
+            {
+                int minRope1 = minHeap.Pop();
+                int minRope2 = minHeap.Pop();
+                int newRopeCost = minRope1 + minRope2;
+                totalCost += newRopeCost;
+                minHeap.Add(newRopeCost);
+
+            } while (minHeap.GetSize() > 1);
+
+            return totalCost;
+        }
+    }
+}
---
+++ Project_6/XUnitTestAlgorithm/Heap/MinimiseCostConnectRopes_Test.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm.Heap;
+using Xunit;
+
+namespace XUnitTestAlgorithm.Heap
+{
+    public class MinimiseCostConnectRopes_Test
+    {
+        [Fact]
+        public void MinimiseCostConnectRopes_Test1()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int expectedResult = 33;
+            MinimiseCostConnectRopes minimiseCostConnectRopes = new();
+            int result = minimiseCostConnectRopes.GetMinCostUsedPriorityQueue(arr, arr.Length);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void MinimiseCostConnectRopes_Test2()
+        {
+            int[] arr = { 4, 3, 2, 6 };
+            int expectedResult = 29;
+            MinimiseCostConnectRopes minimiseCostConnectRopes = new();
+            int result = minimiseCostConnectRopes.GetMinCostUsedPriorityQueue(arr, arr.Length);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void AmazonMediaEncoder_Test1()
+        {
+            int[] files = { 4, 8, 6, 12 };
+            int numOfSubfiles = 4;
+            int expectedResult = 58;
+            MinimiseCostConnectRopes minimiseCostConnectRopes = new();
+            int result = minimiseCostConnectRopes.GetMinCostUsedPriorityQueue(files, numOfSubfiles);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void MinimiseCostConnectRopes_MinHeap_Test1()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int expectedResult = 33;
+            MinimiseCostConnectRopes minimiseCostConnectRopes = new();
+            int result = minimiseCostConnectRopes.GetMinCostUseMinHeap(arr, arr.Length);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void MinimiseCostConnectRopes_minHeap_Test2()
+        {
+            int[] arr = { 4, 3, 2, 6 };
+            int expectedResult = 29;
+            MinimiseCostConnectRopes minimiseCostConnectRopes = new();
+            int result = minimiseCostConnectRopes.GetMinCostUseMinHeap(arr, arr.Length);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void AmazonMediaEncoder_MinHeap_Test1()
+        {
+            int[] files = { 4, 8, 6, 12 };
+            int numOfSubfiles = 4;
+            int expectedResult = 58;
+            MinimiseCostConnectRopes minimiseCostConnectRopes = new();
+            int result = minimiseCostConnectRopes.GetMinCostUseMinHeap(files, numOfSubfiles);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
